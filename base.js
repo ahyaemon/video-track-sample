17c0ac5d55abc39d8b3969ed4d74549b7ba1ea69
ahyaemon <iamahyaemon@gmail.com>
2021-07-05 08:37:48 +0900

    update

--- base.js
+++ base.js
@@ -1,4 +1,34 @@
-window.onload = () => {
-    console.log('hello')
+function s(selector) {
+    return document.querySelector(selector)
+}
 
+function sa(selector) {
+    return document.querySelectorAll(selector)
+}
+
+function createVideoElement(filename) {
+    const videoElement = document.createElement('video')
+    videoElement.src = filename
+    return videoElement
+}
+
+function createTrackElement(subtitle) {
+    const trackElement = document.createElement('track')
+    trackElement.src = subtitle
+    trackElement.default = true
+    return trackElement
+}
+
+window.onload = () => {
+    const videoList = [...sa('.video-data')]
+        .map(item => { return { filename: item.dataset.filename, subtitle: item.dataset.subtitle } })
+        .map(item => {
+            const video = createVideoElement(item.filename)
+            const track = createTrackElement(item.subtitle)
+            video.appendChild(track)
+            return video
+        })
+
+    const videoContainer = s('.video-container')
+    videoContainer.appendChild(videoList[0])
 }
