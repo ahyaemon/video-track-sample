3b5e326d06feefaec8aecabd4811f6fc90504d7e
ahyaemon <iamahyaemon@gmail.com>
2021-07-07 09:06:06 +0900

    update

--- base.js
+++ base.js
@@ -6,62 +6,72 @@ function sa(selector) {
     return document.querySelectorAll(selector)
 }
 
-function createVideoElement(filename) {
-    const videoElement = document.createElement('video')
-    videoElement.src = filename
-    videoElement.playsInline = true
-    return videoElement
+function createSourceElement(filename) {
+    const e = document.createElement('source')
+    e.src = filename
+    return e
 }
 
 function createTrackElement(subtitle) {
-    const trackElement = document.createElement('track')
-    trackElement.src = subtitle
-    trackElement.default = true
-    return trackElement
+    const e = document.createElement('track')
+    e.src = subtitle
+    e.default = true
+    return e
+}
+
+function showTrack(e) {
+    e.textTracks[0].mode = "showing"
+}
+
+function hideTrack(e) {
+    e.textTracks[0].mode = "disabled"
 }
 
 window.onload = () => {
     const videoList = [...sa('.video-data')]
-        .map(item => { return { filename: item.dataset.filename, subtitle: item.dataset.subtitle } })
         .map(item => {
-            const video = createVideoElement(item.filename)
-            const track = createTrackElement(item.subtitle)
-            video.appendChild(track)
-            return video
+            return {
+                filename: item.dataset.filename,
+                source: createSourceElement(item.dataset.filename),
+                track: createTrackElement(item.dataset.subtitle),
+            }
         })
     let currentVideoIndex = 0
     let currentVideo = videoList[currentVideoIndex]
     let isSubtitleShowing = false
 
-    const videoContainer = s('.video-container')
-    videoContainer.appendChild(videoList[currentVideoIndex])
+    const video = s('.video')
+    video.src = currentVideo.filename
+    video.appendChild(currentVideo.track)
+    hideTrack(video)
 
     const playButton = s('.play-button')
     playButton.onclick = () => {
+        video.play()
         if (isSubtitleShowing) {
-            currentVideo.textTracks[0].mode = "showing"
+            hideTrack(video)
+            showTrack(video)
         } else {
-            currentVideo.textTracks[0].mode = "disabled"
+            showTrack(video)
+            hideTrack(video)
         }
-        console.log(currentVideo)
-        currentVideo.play()
     }
 
     const pauseButton = s('.pause-button')
     pauseButton.onclick = () => {
-        currentVideo.pause()
+        video.pause()
     }
 
     const subtitleOnButton = s('.subtitle-on-button')
     subtitleOnButton.onclick = () => {
         isSubtitleShowing = true
-        currentVideo.textTracks[0].mode = "showing"
+        showTrack(video)
     }
 
     const subtitleOffButton = s('.subtitle-off-button')
     subtitleOffButton.onclick = () => {
         isSubtitleShowing = false
-        currentVideo.textTracks[0].mode = "disabled"
+        hideTrack(video)
     }
 
     const nextButton = s('.next-button')
@@ -69,10 +79,12 @@ window.onload = () => {
         if (currentVideoIndex === videoList.length - 1) {
             return
         }
-        currentVideo.remove()
+        // currentVideo.source.remove()
+        currentVideo.track.remove()
         currentVideoIndex += 1
         currentVideo = videoList[currentVideoIndex]
-        videoContainer.appendChild(currentVideo)
+        video.src = currentVideo.filename
+        video.appendChild(currentVideo.track)
     }
 
     const prevButton = s('.prev-button')
@@ -80,9 +92,11 @@ window.onload = () => {
         if (currentVideoIndex === 0) {
             return
         }
-        currentVideo.remove()
+        // currentVideo.source.remove()
+        currentVideo.track.remove()
         currentVideoIndex -= 1
         currentVideo = videoList[currentVideoIndex]
-        videoContainer.appendChild(currentVideo)
+        video.src = currentVideo.filename
+        video.appendChild(currentVideo.track)
     }
 }
